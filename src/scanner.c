c632af127feabcc008417d048460bd801959e725
Aymane El Maini <elmainiaymane03@gmail.com>
2025-12-10 22:09:09 +0100

    feat: implement scanning for other complicated tokens (strings, numbers, identifiers)
    - hard work was at indefiying keywords reserved by the langauge, early
    in klox i implement this by just defining a map of this keywords (and
    who cares about performance and all :joy) but here in clox i implement
    this using DFA -> aka deterministic finite automaton

--- src/scanner.c
+++ src/scanner.c
@@ -21,6 +21,16 @@ void init_scanner(const char *source)
 	scanner.line = 1;
 }
 
+static bool is_alpha(const char c)
+{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+static bool is_digit(const char c)
+{
+	return c >= '0' && c <= '9';
+}
+
 static bool is_at_end()
 {
 	return *scanner.current == '\0';
@@ -102,6 +112,104 @@ static void skip_white_space()
 	}
 }
 
+static TokenType check_keyword(int start, int length, const char *rest,
+			       const TokenType type)
+{
+	if (scanner.current - scanner.start == start + length &&
+	    memcmp(scanner.start + start, rest, length) == 0) {
+		return type;
+	}
+	return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifier_type()
+{
+	switch (scanner.start[0]) {
+	case 'a':
+		return check_keyword(1, 2, "nd", TOKEN_AND);
+	case 'c':
+		return check_keyword(1, 4, "lass", TOKEN_CLASS);
+	case 'e':
+		return check_keyword(1, 3, "lse", TOKEN_ELSE);
+	case 'f':
+		if (scanner.current - scanner.start > 1) {
+			switch (scanner.start[1]) {
+			case 'a':
+				return check_keyword(2, 3, "lse", TOKEN_FALSE);
+			case 'o':
+				return check_keyword(2, 1, "r", TOKEN_FOR);
+			case 'u':
+				return check_keyword(2, 1, "n", TOKEN_FUN);
+			}
+		}
+		break;
+	case 'i':
+		return check_keyword(1, 1, "f", TOKEN_IF);
+	case 'n':
+		return check_keyword(1, 2, "il", TOKEN_NIL);
+	case 'o':
+		return check_keyword(1, 1, "r", TOKEN_OR);
+	case 'p':
+		return check_keyword(1, 4, "rint", TOKEN_PRINT);
+	case 'r':
+		return check_keyword(1, 5, "eturn", TOKEN_RETURN);
+	case 's':
+		return check_keyword(1, 4, "uper", TO0KEN_SUPER);
+
+	case 't':
+		if (scanner.current - scanner.start > 1) {
+			switch (scanner.start[1]) {
+			case 'h':
+				return check_keyword(2, 2, "is", TOKEN_THIS);
+			case 'r':
+				return check_keyword(2, 2, "ue", TOKEN_TRUE);
+			}
+		}
+	case 'v':
+		return check_keyword(1, 2, "ar", TOKEN_VAR);
+	case 'w':
+		return check_keyword(1, 4, "hile", TOKEN_WHILE);
+	}
+	return TOKEN_IDENTIFIER;
+}
+
+static Token consume_identifier()
+{
+	while (is_alpha(peek()) || is_digit(peek()))
+		advance();
+
+	return make_token(identifier_type());
+}
+
+static Token consume_number()
+{
+	while (is_digit(peek()))
+		advance();
+
+	if (peek() == '.' && is_digit(peek_next())) {
+		advance();
+
+		while (is_digit(peek()))
+			advance();
+	}
+	return make_token(TOKEN_NUMBER);
+}
+
+static Token consume_string()
+{
+	while (peek() != '"' && !is_at_end()) {
+		if (peek() == '\n')
+			scanner.line++;
+		advance();
+	}
+
+	if (peek() == '\n')
+		return error_token("unterminated string");
+
+	advance();
+	return make_token(TOKEN_STRING);
+}
+
 Token scan_token()
 {
 	skip_white_space();
@@ -111,6 +219,13 @@ Token scan_token()
 		return make_token(TOKEN_EOF);
 
 	char c = advance();
+
+	if (is_alpha(c))
+		return consume_identifier();
+
+	if (is_digit(c))
+		return consume_number();
+
 	switch (c) {
 	case '(':
 		return make_token(TOKEN_LEFT_PAREN);
@@ -143,6 +258,8 @@ Token scan_token()
 	case '>':
 		return make_token(match('=') ? TOKEN_GREATER_EQUAL
 					     : TOKEN_GREATER);
+	case '"':
+		return consume_string();
 	}
 
 	return error_token("Unexpected Character \n");

a928c13230dc478dc9541554f3b8e756f984a3b2
Aymane El Maini <elmainiaymane03@gmail.com>
2025-12-10 01:41:54 +0100

    feat: start token scanning

--- src/compiler.c
+++ src/compiler.c
@@ -1,8 +1,25 @@
 #include "compiler.h"
 #include "common.h"
 #include "scanner.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 void compile(const char *source)
 {
 	initScanner(source);
+
+    int line = -1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("    %4d", token.line);
+            line = token.line;
+        } else {
+            printf("      | ");
+        }
+
+        printf("%2d '%.*s' \n", token.line, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
--- src/scanner.c
+++ src/scanner.c
@@ -1,5 +1,7 @@
+#include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <uchar.h>
 
 #include "common.h"
 #include "scanner.h"
@@ -17,4 +19,89 @@ void initScanner(const char *source)
 	scanner.current = source;
 	scanner.start = source;
 	scanner.line = 1;
+}
+
+static bool isAtend()
+{
+	return *scanner.current == '\0';
+}
+
+static char advance()
+{
+	scanner.current++;
+	return scanner.current[-1];
+}
+
+static bool match(const char expected)
+{
+	if (isAtend())
+		return false;
+	if (*scanner.current != expected)
+		return false;
+
+	scanner.current++;
+	return true;
+}
+
+static Token makeToken(TokenType type)
+{
+	Token token = {.type = type,
+		       .start = scanner.start,
+		       .length = (int)(scanner.current - scanner.start),
+		       .line = scanner.line};
+	return token;
+}
+
+static Token errorToken(const char *message)
+{
+	Token token = {.type = TOKEN_ERROR,
+		       .start = message,
+		       .length = (int)strlen(message),
+		       .line = scanner.line};
+
+	return token;
+}
+
+Token scanToken()
+{
+	scanner.start = scanner.current;
+
+	if (isAtend())
+		return makeToken(TOKEN_EOF);
+
+	char c = advance();
+	switch (c) {
+	case '(':
+		return makeToken(TOKEN_LEFT_PAREN);
+	case ')':
+		return makeToken(TOKEN_RIGHT_PAREN);
+	case '{':
+		return makeToken(TOKEN_LEFT_BRACE);
+	case '}':
+		return makeToken(TOKEN_RIGHT_BRACE);
+	case ';':
+		return makeToken(TOKEN_SEMICOLON);
+	case ',':
+		return makeToken(TOKEN_COMMA);
+	case '.':
+		return makeToken(TOKEN_DOT);
+	case '-':
+		return makeToken(TOKEN_MINUS);
+	case '+':
+		return makeToken(TOKEN_PLUS);
+	case '/':
+		return makeToken(TOKEN_SLASH);
+	case '*':
+		return makeToken(TOKEN_STAR);
+	case '!':
+		return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+	case '=':
+        return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+        return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+        return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+	}
+
+	return errorToken("Unexpected Character \n");
 }

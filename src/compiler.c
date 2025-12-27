91e40674e192c332d394ce15126af90928a9f486
Aymane El Maini <elmainiaymane03@gmail.com>
2025-12-27 22:20:13 +0100

    feat: add for loops support

--- src/compiler.c
+++ src/compiler.c
@@ -615,6 +615,60 @@ static void expression_statement()
     emit_byte(OP_POP);
 }
 
+static void for_satement()
+{
+    begin_scope();
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'");
+    if (match(TOKEN_SEMICOLON))
+    {
+    }
+    else if (match(TOKEN_VAR))
+    {
+        var_declaration();
+    }
+    else
+    {
+        expression_statement();
+    }
+
+    int loop_start = current_chunk()->count;
+
+    int exit_jump = -1;
+    if (!match(TOKEN_SEMICOLON))
+    {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition");
+
+        exit_jump = emit_jump(OP_JUMP_IF_FALSE);
+        emit_byte(OP_POP);
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN))
+    {
+        int jump_body = emit_jump(OP_JUMP);
+
+        int increment_start = current_chunk()->count;
+        expression();
+        emit_byte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses");
+
+        emit_loop(loop_start);
+        loop_start = increment_start;
+        patch_jump(jump_body);
+    }
+
+    statement();
+
+    emit_loop(loop_start);
+
+    if (exit_jump != -1)
+    {
+        patch_jump(exit_jump);
+        emit_byte(OP_POP);
+    }
+    end_scope();
+}
+
 static void if_statement()
 {
     consume(TOKEN_LEFT_PAREN, "Expect '(' after if statement");
@@ -702,17 +756,13 @@ static void declaration()
 static void statement()
 {
     if (match(TOKEN_PRINT))
-    {
         print_statement();
-    }
+    else if (match(TOKEN_FOR))
+        for_statement();
     else if (match(TOKEN_IF))
-    {
         if_statement();
-    }
     else if (match(TOKEN_WHILE))
-    {
         while_statement();
-    }
     else if (match(TOKEN_LEFT_BRACE))
     {
         begin_scope();
@@ -720,9 +770,7 @@ static void statement()
         end_scope();
     }
     else
-    {
         expression_statement();
-    }
 }
 
 static inline void init_parser()
